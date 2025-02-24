47e20f3ff0ececf62f8d3a46d0e0ce7024fc9dd5
Varun Kumar <varunkumar1329@gmail.com>
2025-02-24 18:39:49 +0530

    commit it

--- shop/views.py
+++ shop/views.py
@@ -107,7 +107,6 @@ def register(request):
       return redirect('/login')
   return render(request,"shop/register.html",{'form':form})
  
-
 def collections(request):
   catagory=Catagory.objects.filter(status=0)
   return render(request,"shop/collections.html",{"catagory":catagory})
